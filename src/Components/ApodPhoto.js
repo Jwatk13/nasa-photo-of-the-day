e958b235a592d79b93b272a5d56c299074ba61fa
Joseph Watkins <josephxwatkins@gmail.com>
2022-05-19 17:41:31 -0400

    Added styling to the ApodPhoto.js file

--- src/Components/ApodPhoto.js
+++ src/Components/ApodPhoto.js
@@ -1,17 +1,62 @@
 //STEP 11 -- IMPORT REACT AND STATE HOOKS IF NEEDED
 import react from 'react';
+import styled, { keyframes } from 'styled-components';
+
+
+const StyledApod = styled.div`
+    background: linear-gradient(darkblue, darkred);
+    min-height: 100vh;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    
+    h2 {
+        color: ${props => props.theme.white};
+        padding: 2px;
+        border-bottom: dotted white 5px;
+    }
+
+    p {
+        color: ${props => props.theme.white};
+    }
+
+    .date {
+        font-weight: 600;
+    }
+    
+    img {
+      max-width: 65%;
+      border: double white 10px;
+      border-radius: 8px;
+    }
+    
+    .copyright {
+        color: ${props => props.theme.white};
+        font-size: small;
+        border-bottom: solid black 4px;
+        display: flex;
+        justify-content: center;
+        padding-bottom: 8px;
+    }
+
+    .explanation {
+        padding: 4em;
+        border-top: solid black 2px;
+        border-bottom: solid black 2px;
+    }
+`
 
 //STEP 12 -- CREATE OUR COMPONENT FUNCTION FOR JS, AND SET THE DEFAULT EXPORT
 //STEP 13 -- SETUP THE ELEMENTS THAT I WANT FOR THIS APP
 const ApodPhoto = (props) => {
     return (
-        <div className='apod-photo-wrapper'>
+        <StyledApod className='apod-photo-wrapper'>
             <h2>{props.photo.title}</h2>
-            <p>{props.photo.date}</p>
+            <p className='date'>{props.photo.date}</p>
             <img src={props.photo.hdurl} />
-            <p>{props.photo.explanation}</p>
-            <p>copyright - {props.photo.copyright}</p>
-        </div>
+            <p className='explanation'>{props.photo.explanation}</p>
+            <p className='copyright'>copyright - {props.photo.copyright}</p>
+        </StyledApod>
     )
 }
 
---
+++ src/Theme/index.js
@@ -0,0 +1,9 @@
+//theme object setup
+export default {
+    primaryColor: "darkblue",
+    secondaryColor: "crimson",
+    tertiaryColer: "darkred",
+    black: 'black',
+    white: "white",
+    breakpointMobile: "(max-width: 500px)"
+}
